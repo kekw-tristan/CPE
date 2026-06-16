e1f5f3b97d8a2c762fe277d8f64e690278efea84
Tristan <tristanzocktgfx@gmail.com>
2026-06-16 19:51:02 +0200

    added frame resource

---
+++ engine/src/graphics/vulkan/vulkanFrame.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <graphics/vulkan/vulkanBuffer.h>
+
+#include <vulkan/vulkan.h>
+
+namespace Engine::GFX
+{
+    struct sVulkanFrame
+    {
+        VkCommandBuffer pCommandBuffer = VK_NULL_HANDLE;
+        VkFence         inFlightFence  = VK_NULL_HANDLE;
+        
+        VkSemaphore imageAvailableSemaphore = VK_NULL_HANDLE; 
+        VkSemaphore renderFinishedSemaphore = VK_NULL_HANDLE;
+
+        cVulkanBuffer pCameraBuffer;
+    };
+}
